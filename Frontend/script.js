ad0c0d63a6754b96968263f4cffb215be9678509
Hikko218 <heikor93@outlook.de>
2025-07-03 16:59:44 +0200

    Added Login/Logout function, closes #1

--- Frontend/script.js
+++ Frontend/script.js
@@ -71,7 +71,7 @@ async function showSection(sectionId) {
   // Load projects content if the section is 'projects'
   if (sectionId === 'projects') {
     try {
-      const res = await fetch('http://localhost:3000/api/content/projects', {
+      const res = await fetch('capi/content/projects', {
         credentials: 'include'
       });
       const projects = await res.json();
@@ -271,22 +271,50 @@ function showPopupError(bool) {
 // Event listeners for Admin login form
 document.getElementById('adminLoginForm').addEventListener('submit', async (e) => {
   e.preventDefault();
-  const username = document.getElementById('adminUser').value;
-  const password = document.getElementById('adminPass').value;
+  const usernameInput = document.getElementById('adminUser');
+  const passwordInput = document.getElementById('adminPass');
+  const adminLogin = document.getElementById('adminLoginBtn');
 
-  const res = await fetch('/api/admin/login', {
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  const res = await fetch('http://localhost:3000/api/admin/login', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',     // Include cookies for session management
     body: JSON.stringify({ username, password })
   });
 
   if (res.ok) {
-    const data = await res.json();
-    localStorage.setItem('token', data.token);
     alert("Login successful");
-    // Redirect or load edit UI
+    adminLogin.innerHTML = "Logout";
+    adminLogin.className = "logout-btn";      // Change button ID for logout
+    adminLogin.onclick = () => {logoutAdmin()};
+    showSection('home');
+    
+    usernameInput.value = '';                      // Clear input fields
+    passwordInput.value = '';
   } else {
     alert("Login failed");
   }
 });
+
+// Logout function for admin
+async function logoutAdmin() {
+  const res = await fetch('http://localhost:3000/api/admin/logout', {
+    method: 'POST',
+    credentials: 'include' // Include cookies for session management
+  });
+
+  if (res.ok) {
+    alert("Logout successful");
+    const adminLogout = document.getElementById('adminLoginBtn');
+    adminLogout.innerHTML = "Login";
+    adminLogout.className = "topmenu"; // Change button ID back to login
+    adminLogout.onclick = () => {showLoginSection('Login')};
+    showSection('Home'); 
+  } else {
+    alert("Logout failed");
+  }
+}
 
